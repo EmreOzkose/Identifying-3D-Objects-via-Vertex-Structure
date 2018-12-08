5f3c9f26a0aefa62e09cfb915298fd3867432a85
furkancaglayan <furkancaglayan15@hotmail.com>
2018-12-08 21:29:30 +0300

    Camera object added.

--- OpenGLExperimental/main.cpp
+++ OpenGLExperimental/main.cpp
@@ -1,18 +1,26 @@
+/*
+FPS control demo in GLUT by Nghia Ho
+
+SPACEBAR - toggle FPS control
+W,A,S,D - to move
+mouse - look around, inverted mouse
+left/right mouse - fly up/down
+ESC - quit
+
+*/
+
 #include <Angel_commons/Angel.h>
-#include <string>
+#include "Camera.h"
+#define width 900
+#define height 900
 #include "Program.h"
 #include "Window.h"
 #include "Renderer.h"
-#include "Vector3.h"
-#include "Object.h"
+#define WINDOWNAME "Assignment-2"
+using namespace std;
+vec3 vertices[] = { vec3(1,1,-5),vec3(0,1,-5),vec3(.5f,4,-5)
 
-
-#define WINDOWNAME "PROJECT"
-
-vec3 vertices[] = { vec3(.1f,0,5),
-				  vec3(-.1f,0,5),
-				  vec3(0,.1f,4) };
-
+};
 void init()
 {
 	GLuint vao;
@@ -23,7 +31,7 @@ void init()
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
-		GL_STATIC_DRAW);
+		GL_DYNAMIC_DRAW);
 	// Load shaders and use the resulting shader program
 	GLuint program = InitShader("vshader.glsl", "fshader.glsl");
 	glUseProgram(program);
@@ -32,61 +40,216 @@ void init()
 	glEnableVertexAttribArray(vPosition);
 	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	// Paint the background black
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-	glClearDepth(1.0f); // Set background depth to farthest
-	// In display()
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//Set the clearing(background) color to black(R = 0, G = 0, B = 0) and opaque(A = 1), and the clearing(background) depth to the farthest(Z = 1).In display(), we invoke glClear() to clear the color and depth buffer, with the clearing color and depth, before rendering the graphics. (Besides the color buffer and depth buffer, OpenGL also maintains an accumulation buffer and a stencil buffer which shall be discussed later.)
-
-		glEnable(GL_DEPTH_TEST); // Enable depth testing for z-culling
-	glDepthFunc(GL_LEQUAL); // Set the type of depth-test
-//	We need to enable depth - test to remove the hidden surface, and set the function used for the depth test.
-
-		glShadeModel(GL_SMOOTH); // Enable smooth shading
-//We enable smooth shading in color transition.The alternative is GL_FLAT.Try it out and see the difference.
-
-		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // Nice perspective corrections
-//	In graphics rendering, there is often a trade - off between processing speed and visual quality.We can use glHint() to decide on the trade - off.In this case, we ask for the best perspective correction, which may involve more processing.The default is GL_DONT_CARE.
-	//
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
 }
-void Render(void) {
+void Display();
+void Reshape(int w, int h);
+void Keyboard(unsigned char key, int x, int y);
+void KeyboardUp(unsigned char key, int x, int y);
+void MouseMotion(int x, int y);
+void Mouse(int button, int state, int x, int y);
+void Timer(int value);
+void Idle();
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	
-	glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW);
-	GLfloat aspect = (GLfloat)1200 / (GLfloat)1200; // Compute aspect ratio of window
-	glLoadIdentity();
-	glMatrixMode(GL_PROJECTION); // To operate on the Projection matrix
+void Grid();
 
-	gluPerspective(45.0f, aspect, 0.1f, 100.0f); // Perspective projection: fovy, aspect, near, far
-	gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
-	glLoadIdentity(); // Reset
-	// draw curve
-	glDrawArrays(GL_LINE_LOOP, 0, 3);
-	glutSwapBuffers();
-	glFlush();
-}
+Camera g_camera;
+bool g_key[256];
+bool g_shift_down = false;
+bool g_fps_mode = false;
+int g_viewport_width = 0;
+int g_viewport_height = 0;
+bool g_mouse_left_down = false;
+bool g_mouse_right_down = false;
+
+// Movement settings
+const float g_translation_speed = 0.05;
+const float g_rotation_speed = M_PI / 180 * 0.2;
 
 int main(int argc, char **argv) {
-
 	Program mainProgram;
-	mainProgram.Init(argc,argv);
-	
+	mainProgram.Init(argc, argv);
 	Window window;
-	unsigned int displayMode = GLUT_RGBA | GLUT_SINGLE | GLUT_DOUBLE;
-	window.Init(displayMode,vec2(300,300),vec2(1200, 1200));
+	unsigned int displayMode = GLUT_DOUBLE | GLUT_DEPTH;
+	window.Init(displayMode, vec2(200, 200), vec2(width, height));
 	window.Show(WINDOWNAME);
+	
+	glutIgnoreKeyRepeat(1);
 
-	/*string name = "Furkan";
-	Object obj(name);*/
-	init();
+	glutDisplayFunc(Display);
+	glutIdleFunc(Display);
+	glutReshapeFunc(Reshape);
+	glutMouseFunc(Mouse);
+	glutMotionFunc(MouseMotion);
+	glutPassiveMotionFunc(MouseMotion);
+	glutKeyboardFunc(Keyboard);
+	glutKeyboardUpFunc(KeyboardUp);
+	glutIdleFunc(Idle);
 
-	Renderer::Display(Render);
-
-	// enter GLUT event processing cycle
+	glutTimerFunc(1, Timer, 0);
 	glutMainLoop();
 
-	return 1;
+
+	return 0;
+}
+
+void Grid()
+{
+	glPushMatrix();
+	glColor3f(1, 1, 1);
+
+	for (int i = -50; i < 50; i++) {
+		glBegin(GL_LINES);
+		glVertex3f(i, 0, -50);
+		glVertex3f(i, 0, 50);
+		glEnd();
+	}
+
+	for (int i = -50; i < 50; i++) {
+		glBegin(GL_LINES);
+		glVertex3f(-50, 0, i);
+		glVertex3f(50, 0, i);
+		glEnd();
+	}
+
+	glPopMatrix();
+}
+
+void Display(void) {
+	glClearColor(1.0, 1.0, 0.0, 1.0); //clear the screen to black
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear the color buffer and the depth buffer
+	glLoadIdentity();
+	g_camera.Refresh();
+
+	glColor3f(0, 1, 0);
+	glutWireTeapot(0.5);
+	glutWireSphere(2,4,2);
+	Grid();
+	
+	glutSwapBuffers(); //swap the buffers
+}
+
+void Reshape(int w, int h) {
+	g_viewport_width = w;
+	g_viewport_height = h;
+
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h); //set the viewport to the current window specifications
+	glMatrixMode(GL_PROJECTION); //set the matrix to projection
+
+	glLoadIdentity();
+	gluPerspective(60, (GLfloat)w / (GLfloat)h, 0.1, 100.0); //set the perspective (angle of sight, width, height, ,depth)
+	glMatrixMode(GL_MODELVIEW); //set the matrix back to model
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	if (key == 27) {
+		exit(0);
+	}
+
+	if (key == ' ') {
+		g_fps_mode = !g_fps_mode;
+
+		if (g_fps_mode) {
+			glutSetCursor(GLUT_CURSOR_NONE);
+			glutWarpPointer(g_viewport_width / 2, g_viewport_height / 2);
+		}
+		else {
+			glutSetCursor(GLUT_CURSOR_LEFT_ARROW);
+		}
+	}
+
+	if (glutGetModifiers() == GLUT_ACTIVE_SHIFT) {
+		g_shift_down = true;
+	}
+	else {
+		g_shift_down = false;
+	}
+
+	g_key[key] = true;
+}
+
+void KeyboardUp(unsigned char key, int x, int y)
+{
+	g_key[key] = false;
+}
+
+void Timer(int value)
+{
+	if (g_fps_mode) {
+		if (g_key['w'] || g_key['W']) {
+			g_camera.Move(g_translation_speed);
+		}
+		else if (g_key['s'] || g_key['S']) {
+			g_camera.Move(-g_translation_speed);
+		}
+		else if (g_key['a'] || g_key['A']) {
+			g_camera.Strafe(g_translation_speed);
+		}
+		else if (g_key['d'] || g_key['D']) {
+			g_camera.Strafe(-g_translation_speed);
+		}
+		else if (g_mouse_left_down) {
+			g_camera.Fly(-g_translation_speed);
+		}
+		else if (g_mouse_right_down) {
+			g_camera.Fly(g_translation_speed);
+		}
+	}
+
+	glutTimerFunc(1, Timer, 0);
+}
+
+void Idle()
+{
+	Display();
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+	if (state == GLUT_DOWN) {
+		if (button == GLUT_LEFT_BUTTON) {
+			g_mouse_left_down = true;
+		}
+		else if (button == GLUT_RIGHT_BUTTON) {
+			g_mouse_right_down = true;
+		}
+	}
+	else if (state == GLUT_UP) {
+		if (button == GLUT_LEFT_BUTTON) {
+			g_mouse_left_down = false;
+		}
+		else if (button == GLUT_RIGHT_BUTTON) {
+			g_mouse_right_down = false;
+		}
+	}
+}
+
+void MouseMotion(int x, int y)
+{
+	// This variable is hack to stop glutWarpPointer from triggering an event callback to Mouse(...)
+	// This avoids it being called recursively and hanging up the event loop
+	static bool just_warped = false;
+
+	if (just_warped) {
+		just_warped = false;
+		return;
+	}
+
+	if (g_fps_mode) {
+		int dx = x - g_viewport_width / 2;
+		int dy = y - g_viewport_height / 2;
+
+		if (dx) {
+			g_camera.RotateYaw(g_rotation_speed*dx);
+		}
+
+		if (dy) {
+			g_camera.RotatePitch(g_rotation_speed*dy);
+		}
+
+		glutWarpPointer(g_viewport_width / 2, g_viewport_height / 2);
+
+		just_warped = true;
+	}
 }
