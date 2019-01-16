134cda51d18a501a107d32e6306de13a03d6359b
EmreOzkose <yozkose3@hotmail.com>
2019-01-17 02:05:56 +0300

    added sound manager and a fancy background music :)

--- OpenGLExperimental/Console.cpp
+++ OpenGLExperimental/Console.cpp
@@ -1,35 +1,34 @@
 #pragma once
 #include "Console.h"
+
+
 void Console::SetupConsole()
 {
 	glui_v_subwindow = GLUI_Master.create_glui_subwindow(*application_window, GLUI_SUBWINDOW_LEFT);
 	new GLUI_StaticText(glui_v_subwindow, "OpenGL Experimental\n");
 	new GLUI_Separator(glui_v_subwindow);
 
-	glui_v_panel_main = new GLUI_Panel(glui_v_subwindow, "Variables", GLUI_PANEL_EMBOSSED);
+	glui_v_panel_parameters = new GLUI_Panel(glui_v_subwindow, "Parameters", GLUI_PANEL_EMBOSSED);
 
-	/*GLUI_Panel* glui_v_panel_ROT = new GLUI_Panel(glui_v_panel_main, "Rotation", GLUI_PANEL_EMBOSSED);
-
-	GLUI_Panel* glui_v_panel_SCALE = new GLUI_Panel(glui_v_panel_main, "Scale", GLUI_PANEL_EMBOSSED);
-	GLUI_Panel* glui_v_panel_SPIRAL = new GLUI_Panel(glui_v_panel_main, "Spiral", GLUI_PANEL_EMBOSSED);*/
 
 	new GLUI_StaticText(glui_v_subwindow, "Press ESC to exit\n");
 
-
-	
-	SelectedObject = new GLUI_StaticText(glui_v_panel_main, selectedText.c_str());
-    //GLUI_EditText *counter_edittext = new GLUI_EditText(glui_v_subwindow, "Count:", 5);
-	//counter_edittext->disable();
+	/*SelectedObject = new GLUI_StaticText(glui_v_panel_parameters, selectedText.c_str());
+    GLUI_EditText *counter_edittext = new GLUI_EditText(glui_v_subwindow, "Count:", 5);
+	*/
 
 	glui_v_subwindow->set_main_gfx_window(*application_window);
 	GLUI_Master.sync_live_all();
 }
+
 
 void Console::Update(GameObject * current)
 {
 	string x = to_string(current->transform.position.x)+" ";
 	string y = to_string(current->transform.position.y) + " ";
 	string z = to_string(current->transform.position.z) + " \n";
+
+
 	selectedText = x+y+z;
 	glui_v_subwindow->sync_live();
 	GLUI_Master.sync_live_all();
--- OpenGLExperimental/Console.h
+++ OpenGLExperimental/Console.h
@@ -9,7 +9,7 @@ public:
 	int *application_window;
 	//GLUI* glui_window;
 	GLUI* glui_v_subwindow; 
-	GLUI_Panel* glui_v_panel_main;
+	GLUI_Panel* glui_v_panel_parameters;
 
 
 	string selectedText;
@@ -17,11 +17,18 @@ public:
 
 	void SetupConsole();
 	void Update(GameObject *current);
-	Console(int &window)
-	{
-		application_window = &window;
-	}
-	Console()
-	{
-	}
+
+	Console(int &window){ application_window = &window; }
+	Console(){}
+
+	// live variables
+	string command_text = "";
+	int wireframe = 0;
+	int bumpmap = 0;
+	
+	GLUI_EditText *text_command;
+	GLUI_EditText *text_num_of_light;
+	GLUI_EditText *text_scene;
+	GLUI_Checkbox *checkbox_wireframe;
+	GLUI_Checkbox *checkbox_bumpmap;
 };
