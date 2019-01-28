60d3fe3fc0b55bdc8a64265d839668f9da92c406
furkancaglayan <furkancaglayan15@hotmail.com>
2019-01-28 22:15:59 +0300

    added soundmanager.

---
+++ OpenGLExperimental/SoundManager.cpp
@@ -0,0 +1,30 @@
+
+#include "SoundManager.h"
+#include <iostream>
+
+using namespace std;
+
+
+int SoundManager::soundCheck() {
+	if (!engine) {
+		cout << "Could not startup engine\n" << endl;
+		return 1; // error starting up the engine
+	}
+	return 0;
+}
+
+ISound *SoundManager::sound_background() {
+	return engine->play2D("audio/background.mp3", true, false, true);
+}
+
+void SoundManager::sound_airplane_departure() {
+	engine->play2D("audio/airplane_departure.mp3", false);
+}
+
+void SoundManager::sound_robot() {
+	engine->play2D("audio/robot.mp3", false);
+}
+
+void SoundManager::stopAllSounds() {
+	engine->stopAllSounds();
+}
